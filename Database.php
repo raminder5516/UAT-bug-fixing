056bafcebce944a28f9b6fecfb5222f77b25d498
matthewjweber <matthewjweber@gmail.com>
2018-04-10 18:25:58 -0400

    database up n running
    database.php is up and working

--- Database.php
+++ Database.php
@@ -1,23 +1,27 @@
-<?php
-class Database
+<?php class Database
 {
-    //private properties
-    private static  $user = 'root';
-    private static $pass = '';
-    private static $dsn = "mysql:host=localhost;dbname=nadeem";
+    
+    //private propertiess
+    private static $user = 'uatbugs';
+    private static $pass = 'mysql123';
+    private static $dsn = "mysql:host=a2plcpnl0830.prod.iad2.secureserver.net;port=3306;dbname=uatbugs";
+    //private static $dsn = "mysql:host=localhost;dbname=final_project";
     private static $db;
-    //get pdo connection
+
+//get pdo connection
     public static function getDb(){
         if(!isset(self::$db)) {
             try {
-                self::$db = new PDO(self::$dsn, self::$user, self::$pass);
+                self::$db = new PDO (self::$dsn, self::$user, self::$pass);
                 self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch (PDOException $e) {
-                $msg = $e->getMessage();
-                include 'customerror.php';
-                exit();
+            } 
+            catch (PDOException $e) {
+            $msg = $e->getMessage();
+            echo $msg;
+            exit();
             }
         }
         return self::$db;
     }
 }
+?>
